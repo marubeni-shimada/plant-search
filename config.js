ade679d408443bf64377d1c5ffde35d591e899d2
marubeni-shimada <ikeda-r@marubeni-co.jp>
2026-05-15 14:01:24 +0900

    Add configuration for GAS Web application

---
+++ config.js
@@ -0,0 +1,8 @@
+// GAS Webアプリのデプロイ後、ここを書き換えてください
+window.APP_CONFIG = {
+  // GAS Webアプリのデプロイ URL(末尾が /exec)
+  API_URL: 'https://script.google.com/macros/s/AKfycbzfBvUdj_yp7RMv7vHBLhkOFP8GXNjKrkk-_jhNk4kQsBRbRn_7dLtxISHl9ogmXBn4uA/exec',
+
+  // 管理画面のURL(GASのデプロイURL + ?page=admin)
+  ADMIN_URL: 'https://script.google.com/macros/s/AKfycbzfBvUdj_yp7RMv7vHBLhkOFP8GXNjKrkk-_jhNk4kQsBRbRn_7dLtxISHl9ogmXBn4uA/exec?page=admin'
+};
